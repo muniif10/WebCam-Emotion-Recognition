b5c20731d826d82c49a677d11fda382e1092d07a
Muniif <muniif10@gmail.com>
2024-01-09 20:49:35 +0800

    Simplify

--- app.py
+++ app.py
@@ -62,9 +62,10 @@ class Faceemotion(VideoTransformerBase):
                 roi = img_to_array(roi)
                 roi = np.expand_dims(roi, axis=0)
                 prediction = classifier.predict(roi)[0]
+                print(prediction)
                 maxindex = int(np.argmax(prediction))
                 finalout = emotion_dict[maxindex]
-                output = str(finalout)
+                output = str(finalout) + " " + "{:,.0%}".format(prediction[maxindex])
             label_position = (x, y)
             cv2.putText(
                 img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2
@@ -75,62 +76,16 @@ class Faceemotion(VideoTransformerBase):
 
 def main():
     # Face Analysis Application #
-    st.title("Real Time Face Emotion Detection Application")
-    activiteis = ["Home", "Webcam Face Detection", "About"]
-    choice = st.sidebar.selectbox("Select Activity", activiteis)
-    st.sidebar.markdown(
-        """ Developed by Mohammad Juned Khan    
-            Email : Mohammad.juned.z.khan@gmail.com  
-            [LinkedIn] (https://www.linkedin.com/in/md-juned-khan)"""
+    st.title("Real Time Face Recognition for Food Industry")
+
+    st.header("Webcam Feed with CV2 haarcascade and CNN model.")
+    st.write("Click on start to use webcam and detect your face emotion")
+    webrtc_streamer(
+        key="example",
+        mode=WebRtcMode.SENDRECV,
+        rtc_configuration=RTC_CONFIGURATION,
+        video_processor_factory=Faceemotion,
     )
-    if choice == "Home":
-        html_temp_home1 = """<div style="background-color:#6D7B8D;padding:10px">
-                                            <h4 style="color:white;text-align:center;">
-                                            Face Emotion detection application using OpenCV, Custom CNN model and Streamlit.</h4>
-                                            </div>
-                                            </br>"""
-        st.markdown(html_temp_home1, unsafe_allow_html=True)
-        st.write(
-            """
-                 The application has two functionalities.
-
-                 1. Real time face detection using web cam feed.
-
-                 2. Real time face emotion recognization.
-
-                 """
-        )
-    elif choice == "Webcam Face Detection":
-        st.header("Webcam Live Feed")
-        st.write("Click on start to use webcam and detect your face emotion")
-        webrtc_streamer(
-            key="example",
-            mode=WebRtcMode.SENDRECV,
-            rtc_configuration=RTC_CONFIGURATION,
-            video_processor_factory=Faceemotion,
-        )
-
-    elif choice == "About":
-        st.subheader("About this app")
-        html_temp_about1 = """<div style="background-color:#6D7B8D;padding:10px">
-                                    <h4 style="color:white;text-align:center;">
-                                    Real time face emotion detection application using OpenCV, Custom Trained CNN model and Streamlit.</h4>
-                                    </div>
-                                    </br>"""
-        st.markdown(html_temp_about1, unsafe_allow_html=True)
-
-        html_temp4 = """
-                             		<div style="background-color:#98AFC7;padding:10px">
-                             		<h4 style="color:white;text-align:center;">This Application is developed by Mohammad Juned Khan using Streamlit Framework, Opencv, Tensorflow and Keras library for demonstration purpose. If you're on LinkedIn and want to connect, just click on the link in sidebar and shoot me a request. If you have any suggestion or wnat to comment just write a mail at Mohammad.juned.z.khan@gmail.com. </h4>
-                             		<h4 style="color:white;text-align:center;">Thanks for Visiting</h4>
-                             		</div>
-                             		<br></br>
-                             		<br></br>"""
-
-        st.markdown(html_temp4, unsafe_allow_html=True)
-
-    else:
-        pass
 
 
 if __name__ == "__main__":
